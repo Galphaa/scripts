13905d548d999c56cd8d993b7532157b147d9937
Sam Hewitt <hewittsamuel@gmail.com>
2016-09-01 13:02:44 -0400

    added comment to  mkv-audio-conversion.sh

--- mkv-audio-conversion.sh
+++ mkv-audio-conversion.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# This is a one-line for loop for batch mkv conversion
+
 for f in *.mkv; do avconv -i "$f" -vcodec copy -acodec aac -scodec copy -strict experimental "${f%.mkv}-out.mkv"; done
